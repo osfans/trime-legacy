4bf7a7aba37c9f1e69481677d449aa034f6d92b9
osfans <waxaca@163.com>
2014-09-29 17:52:59 +0800

    添加SnakeYAML庫

--- Android.mk
+++ Android.mk
@@ -7,9 +7,17 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := \
          $(call all-subdir-java-files)
 
+LOCAL_STATIC_JAVA_LIBRARIES := libsnakeyaml
+
 #LOCAL_AAPT_FLAGS := -0 .db
 
 LOCAL_PACKAGE_NAME := trime
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libsnakeyaml:libs/snakeyaml-1.10-android.jar
+
+include $(BUILD_MULTI_PREBUILT)
 
